f8eb601d6e14d01f0d893f9448014a6bd1b29977
Syakirin Amin <akrinmin@gmail.com>
2017-11-17 19:19:16 +0700

    mimin

---
+++ application/views/forum/admin/dash.php
@@ -0,0 +1,143 @@
+<main>
+
+	  <div class="container-fluid">
+		<div class="row">
+		  <div class="col-sm-12 col-md-6">	
+		<div class="mow">
+		  <div class="mow-head">6 user terakhir <a href="#" class="text-muted float-right small">Lebih banyak</a></div>
+		  <div class="row">
+
+			<div class="col-12">
+			  <div class="mow-user">
+				<img src="/" height=70 width=70 class="mow-user-img rounded-circle"/>
+				
+				<span class="mow-user-name">Akrin</span><br/>
+				<span class="mow-user-date">rabu, 22 agu 2017</span>
+			    <a href="#" class="btn btn-sm btn-outline-danger">Banned</a>
+			  </div>
+			</div><!--col-->
+
+			<div class="col-12">
+			  <div class="mow-user">
+				<img src="/" height=70 width=70 class="mow-user-img rounded-circle"/>
+				<span class="mow-user-name">Akrin</span><br/>
+				<span class="mow-user-date">rabu, 22 agu 2017</span>
+				<a href="#" class="btn btn-sm btn-outline-danger">Banned</a>
+			  </div>
+			</div><!--col-->
+
+			<div class="col-12">
+			  <div class="mow-user">
+				<img src="/" height=70 width=70 class="mow-user-img rounded-circle"/>
+				<span class="mow-user-name">Akrin</span><br/>
+				<span class="mow-user-date">rabu, 22 agu 2017</span>
+				<a href="#" class="btn btn-sm btn-outline-danger">Banned</a>
+			  </div>
+			</div><!--col-->
+
+			
+			<div class="col-12">
+			  <div class="mow-user">
+				<img src="/" height=70 width=70 class="mow-user-img rounded-circle"/>
+				<span class="mow-user-name">Akrin</span><br/>
+				<span class="mow-user-date">rabu, 22 agu 2017</span>
+				<a href="#" class="btn btn-sm btn-outline-danger">Banned</a>
+			  </div>
+			</div><!--col-->
+
+			<div class="col-12">
+			  <div class="mow-user">
+				<img src="/" height=70 width=70 class="mow-user-img rounded-circle"/>
+				<span class="mow-user-name">Akrin</span><br/>
+				<span class="mow-user-date">rabu, 22 agu 2017</span>
+				<a href="#" class="btn btn-sm btn-outline-danger">Banned</a>
+			  </div>
+			</div><!--col-->
+
+			<div class="col-12">
+			  <div class="mow-user">
+				<img src="/" height=70 width=70 class="mow-user-img rounded-circle"/>
+				<span class="mow-user-name">Akrin</span><br/>
+				<span class="mow-user-date">rabu, 22 agu 2017</span>
+				<a href="#" class="btn btn-sm btn-outline-danger">Banned</a>
+			  </div>
+			</div><!--col-->
+			
+		  </div>
+		</div>
+		
+		<!-- wew -->
+		  </div>
+		  
+		  <div class="col-sm-12 col-md-6">	
+			<div class="mow">
+			  <div class="mow-head">6 Posting terakhir <a href="#" class="text-muted float-right small">Lebih banyak</a></div>
+			  <div class="mow-user">
+				<ul class="lili">
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+			         	<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+					<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+					<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+					<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+					<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+					<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				  <li class="list-inline-b">
+					<a href="#" class="mow-link">Varant punya kambing?</a><br/>
+					<span class="text-muted">22 nov 2022</span> . <a href="" class="text-danger">hapus</a>
+				  </li>
+				</ul>
+			</div>
+			</div>
+		  </div>
+		  
+		</div>
+	  </div><!--container-->
+
+	</main>
+	
+		<footer>
+	  <div class="footer-forum">Copyright 2017 - All Right Reserved</div>
+	</footer>
+<script type="text/javascript">
+		$(".drop-body").not('.drop-bodyzz').hide();
+
+  $(".drop-head").click( function(){
+      var current = $(this).next(".drop-body");
+      current.slideToggle(400);
+
+      //---------------------- change plus to -----------------------
+      if( $(this).find("span").html() == '+' ){
+        $(this).find("span").html('-');
+        $(this).addClass('box_head_active');
+
+        //-------------------- close other active box -----------------
+        $(".drop-body").not(current).slideUp(400);
+        $(".drop-head").not($(this)).find("span").html('+');
+      }
+
+      else{
+        $(this).find("span").html('+');
+      }
+  })
+  
+
+</script>
+  </body>
+</html>		
---
+++ application/views/forum/admin/login.php
@@ -0,0 +1,19 @@
+<main>
+	<div class="container-fluid">
+	
+		<div class="miw">
+			<span class="text-muted">User</span><br/>
+			<input type="text" class="form-control" name="user"/>
+			<span class="text-muted">password</span><br/>
+			<input type="password" class="form-control" name="user"/>
+			<br/>
+			<button class="btn btn-primary bll" type="submit">buka</button>
+		</div>
+	
+	</div>
+</main>
+<footer>
+	  <div class="footer-forum">Copyright 2017 - All Right Reserved</div>
+	</footer>
+</body>
+</html>
