bd9f0b883c6fb5fb4c62fe9b25b0d3052d0379bf
Syakirin Amin <akrinmin@gmail.com>
2017-11-20 17:00:39 +0700

    Hmvc simplicity

---
+++ application/views/themes/default.php
@@ -0,0 +1,78 @@
+
+ <!DOCTYPE html>
+      <html lang="en">
+        <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+	<title>ID - Iruna online forum</title>
+	<meta name="description" content="Iruna online forum, tutorial iruna, Production iruna ">
+	<meta name='language' content='id_ID'>
+		<meta name='robots' content='index,follow'>
+			<meta name='HandheldFriendly' content='True'>
+
+ <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+<meta name="google-site-verification" content="KepqXCIws9XJoRrMQkQQZqYnQOPDnFUuH5iIeXT9DNs" />
+<meta name="title" content="ID - Iruna online forum">
+ <meta property="og:url"                content="http://inko-chan.net/" />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="ID" />
+<meta property="og:description"        content="Iruna online forum, tutorial iruna, Production iruna " />
+<meta property="og:image"              content="http://i.cubeupload.com/a6W64O.jpeg" />
+<meta property="fb:app_id" content="2008283499456981"/>
+<meta name="generator" content="WordPress 4.8.3" />
+<link rel="stylesheet" type="text/css" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css' />
+
+
+	<?php
+	/** -- Copy from here -- */
+	if(!empty($meta))
+	foreach($meta as $name=>$content){
+		echo "\n\t\t";
+		?><meta name="<?php echo $name; ?>" content="<?php echo $content; ?>" /><?php
+			 }
+	echo "\n";
+
+	if(!empty($canonical))
+	{
+		echo "\n\t\t";
+		?><link rel="canonical" href="<?php echo $canonical?>" /><?php
+
+	}
+	echo "\n\t";
+
+	foreach($css as $file){
+	 	echo "\n\t\t";
+		?><link rel="stylesheet" href="<?php echo $file; ?>" type="text/css" /><?php
+	} echo "\n\t";
+
+	foreach($js as $file){
+			echo "\n\t\t";
+			?><script src="<?php echo $file; ?>"></script><?php
+	} echo "\n\t";
+
+	/** -- to here -- */
+?>
+  </head>
+  <body>
+	<header>
+	 	<div class="brand"><a href="/">Mobile Iruna Notes</a></div>
+	   <div class="atas position-sticky">
+		<div style="max-width:720px;text-align:left;margin:0 auto;">
+		<div class="kpl-forum">
+			
+		  <a class="list-item" href="http://inko-chan.net/forum">Forum</a>
+  <a class="list-item" href="http://inko-chan.net/login">Panel</a>
+		  <a class="list-item" href="http://inko-chan.net/login">Pesan</a>
+		  <a class="list-item" href="http://inko-chan.net/login">Tulis</a>
+	</div>
+		</div>
+	</div>
+	</header>
+	    <?php echo $output;?>
+	<footer>
+	  <div class="footer-forum">Copyright 2017 - All Right Reserved</div>
+	</footer>
+
+  </body>
+</html>		
