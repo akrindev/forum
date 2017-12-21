668de535124a7313d761135aa969578e7d9bab56
Syakirin Amin <akrinmin@gmail.com>
2017-12-21 22:20:19 +0700

    forum model fix

--- application/models/Forum_model.php
+++ application/models/Forum_model.php
@@ -22,9 +22,16 @@ class Forum_model extends CI_Model
   	return $this->db->get("timeline");
   }
   
+  function get_random_post()
+  {
+  	$this->db->order_by('id','RANDOM');
+	$this->db->limit(4);
+  	return $this->db->get('timeline');
+  }
+  
   function getone($tabel,$slug)
   {
-    return $this->db->query("Select f.id as tlid,f.id_user,f.judul,f.slug,f.isi,f.tags,f.banned,f.date,f.dilihat,u.id,u.email,u.username from $tabel as f inner join users as u where f.id_user = u.id and f.slug ='$slug'");
+    return $this->db->query("Select f.id as tlid,f.id_user,f.judul,f.slug,f.isi,f.tags,f.banned,f.date,f.dilihat,f.pinned,u.id,u.email,u.username from $tabel as f inner join users as u where f.id_user = u.id and f.slug ='$slug'");
   }
   
   
@@ -53,7 +60,7 @@ class Forum_model extends CI_Model
     }
         public function fetch_data_arsip($kat,$limit, $start) {
         
-        $datanya = $this->db->query("select f.id as iid,f.judul,f.slug,f.kat_id,f.dilihat,f.date,ka.id_kat,ka.kat,u.id,u.username from timeline as f join kategori as ka join users as u where f.id_user=u.id and ka.kat = '$kat' and f.kat_id = ka.id_kat order by date desc limit $start, $limit");
+        $datanya = $this->db->query("select f.id as iid,f.judul,f.slug,f.kat_id,f.dilihat,f.date,ka.id_kat,ka.kat,u.id,u.username,u.email from timeline as f join kategori as ka join users as u where f.id_user=u.id and ka.kat = '$kat' and f.kat_id = ka.id_kat order by date desc limit $start, $limit");
       
         if ($datanya->num_rows() > 0) {
             foreach ($datanya->result() as $row) {
@@ -70,7 +77,7 @@ class Forum_model extends CI_Model
     // Fetch data according to per_page limit.
     public function fetch_data($limit, $start) {
         
-        $datanya = $this->db->query("Select f.id as tlid,f.id_user,f.judul,f.slug,f.isi,f.date,f.dilihat,u.id,u.username from timeline as f inner join users as u where f.id_user = u.id order by date desc limit $start, $limit");
+        $datanya = $this->db->query("Select f.id as tlid,f.id_user,f.judul,f.slug,f.isi,f.date,f.dilihat,u.id,u.username,u.email from timeline as f inner join users as u where f.id_user = u.id order by date desc limit $start, $limit");
         
         $query = $this->db->get("timeline");
         if ($query->num_rows() > 0) {
