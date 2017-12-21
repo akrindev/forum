343bbc43fd14bf081f29784b468960bccfbe3642
Syakirin Amin <akrinmin@gmail.com>
2017-12-21 22:18:46 +0700

    sitemap controller

--- application/controllers/Sitemap.php
+++ application/controllers/Sitemap.php
@@ -16,21 +16,13 @@ $data .= '
 
 foreach($this->db->order_by('date','DESC')->get('timeline')->result() as $r)
 { 
-	$up = explode(' ',$r->date);
+	
 	$data .= '
 	<url>
       <loc>';
 
 	$data .= base_url('forum/tl/'.$r->slug.'/');
-	$data .= '</loc>';
-
-   $data .= "   
-<lastmod>$up[0]</lastmod>";
-	$data .= "
-      <changefreq>monthly</changefreq>
-
-      <priority>0.8</priority>
-
+	$data .= "</loc>
    </url>
    ";
 	}
@@ -47,4 +39,42 @@ $this->output
 
 
 	}
+
+function price()
+{
+
+$data = "";
+$data .= '
+<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+';
+   
+
+foreach($this->db->order_by('name','ASC')->get('price')->result() as $r)
+{ 
+	
+	$data .= '
+	<url>
+      <loc>';
+
+	$data .= base_url('harga/item/'.$r->slug.'');
+	$data .= "</loc>
+   </url>
+   ";
+	}
+$data .= "</urlset>";
+
+
+$this->output
+        ->set_status_header(200)
+        ->set_content_type('text/xml', 'utf-8')
+        ->set_output($data)
+        ->_display();
+     exit;
+
+
+
+	}
+
 }
