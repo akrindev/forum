c5bd451d11e5562b2627f9021a8028ced71fa0ee
Syakirin Amin <akrinmin@gmail.com>
2017-12-23 13:28:22 +0700

    config :'3

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://inko-chan.net/';
+$config['base_url'] = 'https://rokoko-iruna.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -378,7 +378,7 @@ $config['encryption_key'] = '1234220899rinkrinnyunchanlopeu';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'ci_riinkrin';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
