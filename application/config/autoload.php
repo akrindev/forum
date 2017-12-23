608cc4e6a03ba3b433050bb2edf68edd790937a5
Syakirin Amin <akrinmin@gmail.com>
2017-12-23 13:28:56 +0700

    auto load language

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Jakarta');
 
 $autoload['packages'] = array();
 
-$autoload['libraries'] = array('database','session','form_validation','email','bbcode','gravatar','recaptcha','pagination','predis');
+$autoload['libraries'] = array('database','session','form_validation','email','bbcode','gravatar','recaptcha','pagination','simple_html_dom');
 
 $autoload['drivers'] = array();
 
@@ -13,7 +13,7 @@ $autoload['helper'] = array('url','form','time_ago','text','tgl_indo','gambar');
 
 $autoload['config'] = array();
 
-$autoload['language'] = array();
+$autoload['language'] = array('msg');
 
 $autoload['model'] = array(
   'user_model' => 'user',
