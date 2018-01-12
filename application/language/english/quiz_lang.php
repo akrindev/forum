925c50dab9848cd781c28952a5d984f1c2c827ed
Syakirin Amin <akrinmin@gmail.com>
2018-01-12 19:00:22 +0700

    added quiz lang English & indonesia

---
+++ application/language/english/quiz_lang.php
@@ -0,0 +1,63 @@
+<?php
+
+$lang['quiz'] = "<strong> --- Quiz --- </strong> <br>
+<p> <br>
+Quiz contains 10 questions. <br>
+there is no time limit. Quiz comes from users who has submitted a question, you can also submit a question <a href='/quiz/newQuiz'> here! </a>
+<br>
+Quiz just to test your knowledge of iruna
+</p>
+
+<strong> --- The Point--- </strong>
+<p> <br>You will get Point when you have finished submitting quiz and automatically added to your current total point. <br> <br>
+if the correct answer is less than 3 you will get +2 point <br>
+if the correct answer 3-5 you will get +5 point <br>
+if the correct answer 6-7 you will get +12 point <br>
+if the correct answer is more than 8 you will get +20 point <br>
+</p>
+
+
+<strong> --- Top Score --- </strong>
+<p> <br>
+User data that appears on the Top score based on the number of top points
+</p>";
+
+$lang['start_quiz'] = "Start Quiz";
+
+$lang['statistic'] = "Statistics Quiz";
+
+$lang['total_quiz'] = "Total quiz";
+$lang['accepted'] = "Accepted";
+$lang['moderate'] = "Pending";
+$lang['newquiz'] = "Submit your quiz!";
+$lang['seerank'] = "See more";
+$lang['notlogin'] = "You have not logged in";
+$lang['select_lang'] = "Choose quiz language";
+$lang['start_q'] = "Quizzes: 10
+<br> <br>
+Quiz randomly selected <br>
+Quiz comes from every user who submits quiz, you can also create quiz <a href='/quiz/newQuiz'> Here </a> <br> <br>";
+
+$lang['question'] = "Question";
+$lang['answer_a'] = "Answer A";
+$lang['answer_b'] = "Answer B";
+$lang['answer_c'] = "Answer C";
+$lang['answer_d'] = "Answer D";
+$lang['correct'] = "Correct Answer";
+$lang['language'] = "Quiz language";
+$lang['rules_q'] = "<ol>
+               <li> Quiz must be about iruna </li>
+               <li> Quiz does not degrade a person / organization </li>
+               <li> Polite language </li>
+               <li> Answer a, b, c, d must be filled / can not be empty </li>
+               <li> Correct first before submitting quiz </li>
+               <li> Quiz can not be edited or changed, contact <a href='mailto:admin@rokoko-iruna.com'> admin@rokoko-iruna.com </a>
+                 if you want to do certain actions </li>
+              </ol>
+              
+              <strong> Quizzes submitted will be in moderation first </strong>";
+$lang['answered'] = "Saved answer";
+$lang['save_q'] = "Save";
+$lang['info_save'] = "Dont forget to <b>Save</b> before go to the next question!";
+$lang['u_sure'] = "Are you sure?";
+$lang['u_will'] = "Submit the quiz?";
---
+++ application/language/indonesia/quiz_lang.php
@@ -0,0 +1,67 @@
+<?php
+
+
+$lang['quiz'] = "   <strong> --- Quiz --- </strong><br>
+<p><br>
+Quiz berisi 10 pertanyaan.<br>
+tidak ada batasan waktu saat mengerjakan. Quiz berasal dari user yang telah mensubmit pertanyaan, kamu juga dapat mensubmit pertanyaan <a href='/quiz/newQuiz'>disini!</a>
+<br>
+Quiz hanya untuk mengetes pengetahuanmu tentang iruna
+</p>
+
+<strong> --- Point yang di dapat --- </strong>
+<p><br>
+Point di dapat ketika kamu telah selesai mensubmit quiz dan otomatis di tambahkan ke total pointmu saat ini.<br><br>
+jika jawaban benar kurang dari 3 kamu medapat +2 point<br>
+jika jawaban benar 3-5 kamu mendapat +5 point<br>
+jika jawaban benar 6-7 kamu mendapat +12 point<br>
+jika jawaban benar lebih dari 8 kamu mendapat +20 point<br>
+</p>
+
+
+<strong> --- Top Score --- </strong>
+<p><br>
+Data user yang tampil pada Top score berdasarkan banyaknya point yang di dapat
+</p>";
+
+$lang['start_quiz'] = "Mulai Quiz";
+
+
+$lang['statistic'] = "Statistik Quiz";
+
+$lang['total_quiz'] = "Jumlah quiz";
+$lang['accepted'] = "Diterima";
+$lang['moderate'] = "Menunggu";
+$lang['newquiz'] = "Buat quiz!";
+$lang['seerank'] = "Lihat semua peringkat";
+$lang['notlogin'] = "kamu belum login";
+$lang['select_lang'] = "Pilih bahasa quiz!";
+$lang['start_q'] = "Kuis sebanyak: 10
+                  <br> <br>
+       Quiz dipilih secara acak <br>
+       Quiz berasal dari setiap user yang mensubmit quiz, kamu juga bisa membuat kuis <a href='/quiz/newQuiz'> Disini </a> <br> <br>";
+
+$lang['question'] = "Pertanyaan";
+$lang['answer_a'] = "Jawaban A";
+$lang['answer_b'] = "Jawaban B";
+$lang['answer_c'] = "Jawaban C";
+$lang['answer_d'] = "Jawaban D";
+$lang['correct'] = "Jawaban benar";
+$lang['language'] = "Bahasa quiz";
+$lang['rules_q'] = "<ol>
+               <li> Quiz bersangkutan dengan iruna </li>
+               <li> Quiz tidak merendahkan seseorang / organisasi </li>
+               <li> Berbahasa yang sopan </li>
+               <li> Jawaban a, b, c, d harus diisi / tidak boleh kosong</li>
+               <li> Koreksi terlebih dahulu sebelum mensubmit quiz</li>
+               <li> Quiz tidak bisa di edit atau di ubah, hubungi <a href='mailto:admin@rokoko-iruna.com?'>admin@rokoko-iruna.com</a>
+                 jika ingin melakukan tindakan tertentu </li>
+              </ol>
+              
+              <strong> Quiz yang telah di submit akan di moderasi terlebih dahulu</strong>";
+
+$lang['answered'] = "Jawaban disimpan";
+$lang['save_q'] = "Simpan";
+$lang['info_save'] = "Jangan lupa click <b>Simpan</b> sebelum pergi ke pertanyaan berikutnya";
+$lang['u_sure'] = "Yakin?";
+$lang['u_will'] = "Kamu akan mensubmit quiz!";
